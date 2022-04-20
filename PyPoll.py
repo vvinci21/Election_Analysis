cf3c79fda4ac9aa01fbfc39071ff5f6ee6f9378c
vvinci21 <vincentrvinci@gmail.com>
2022-04-19 22:38:25 -0400

    Demo

---
+++ PyPoll.py
@@ -0,0 +1,11 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+file_to_load = 'Resources/election_results.csv'
+election_data = open(file_to_load, 'r')
+
+
---
+++ Python_Practice.py
@@ -0,0 +1,9 @@
+print("Hello World");
+title="Frankfurter";
+years=80;
+expert_status=True;
+print(title)
+print("Nick is a pro " + title);
+print("He has been coding for " + str(years));
+print(f"Nick is a pro {title}");
+print(f"He has been coding for {years}");
